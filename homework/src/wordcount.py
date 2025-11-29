19bc7503b57a557375ae8d355db0620e501e2bf7
fhurtadoa <fhurtadoa@unal.edu.co>
2025-11-29 12:45:56 -0500

    TESTS 2

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,11 +1,6 @@
-## Wordcount
-
-# Ejemplo para casos de uso
-# python3 -m homework data/input data/output
-
-
 import argparse
-import sys
+
+from homework.src._internals.read_all_lines import read_all_lines
 
 
 def parse_args():
@@ -24,10 +19,26 @@ def parse_args():
     return parsed_args.input, parsed_args.output
 
 
+def preprocess_lines(lines):
+    pass
+
+
+def split_into_words(preprocessed_lines):
+    pass
+
+
+def count_words(words):
+    pass
+
+
+def write_word_counts(output_folder, word_counts):
+    pass
+
+
 def main():
-    input, output = parse_args()
-    print(f"input: {input}")
-    print(f"output: {output}")
-
-
-# Función test_read_all_lines
+    input_folder, output_folder = parse_args()
+    lines = read_all_lines(input_folder)
+    preprocessed_lines = preprocess_lines(lines)
+    words = split_into_words(preprocessed_lines)
+    word_counts = count_words(words)
+    write_word_counts(output_folder, word_counts)
