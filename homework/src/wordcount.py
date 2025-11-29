4c34bc0982fd3b46363338c6893276f95c831b14
fhurtadoa <fhurtadoa@unal.edu.co>
2025-11-29 12:10:43 -0500

    TESTS
    .

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,1 +1,33 @@
+## Wordcount
 
+# Ejemplo para casos de uso
+# python3 -m homework data/input data/output
+
+
+import argparse
+import sys
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Count Word in files.")
+    parser.add_argument(
+        "input", type=str, help="Path to the input folder containing files to process."
+    )
+    parser.add_argument(
+        "output",
+        type=str,
+        help="Path to the output folder containing files to process.",
+    )
+
+    parsed_args = parser.parse_args()
+
+    return parsed_args.input, parsed_args.output
+
+
+def main():
+    input, output = parse_args()
+    print(f"input: {input}")
+    print(f"output: {output}")
+
+
+# Función test_read_all_lines
